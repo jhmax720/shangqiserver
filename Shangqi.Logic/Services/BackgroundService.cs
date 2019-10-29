0540d393188a8944eefae499e388f584ea1272fc
HENG JIANG <maxpriv22@hotmail.com>
2019-10-29 22:41:52 +1100

    broken reference

--- Shangqi.Logic/Services/BackgroundService.cs
+++ Shangqi.Logic/Services/BackgroundService.cs
@@ -1,31 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
+﻿using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Shangqi.Logic.Services
 {
-    public class BackgroundService: IHostedService
-    {
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
+    //public class BackgroundService: IHostedService
+    //{
+    //    public Task StartAsync(CancellationToken cancellationToken)
+    //    {
             
-            return Task.CompletedTask;
-        }
+    //        return Task.CompletedTask;
+    //    }
 
-        public BackgroundService(ILogger<BackgroundService> logger)
-        {
-            Debug.WriteLine("hello BackgroundService!");
-        }
+    //    public BackgroundService(ILogger<BackgroundService> logger)
+    //    {
+    //        Debug.WriteLine("hello BackgroundService!");
+    //    }
 
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            //New Timer does not have a stop. 
-            return Task.CompletedTask;
-        }
-    }
+    //    public Task StopAsync(CancellationToken cancellationToken)
+    //    {
+    //        //New Timer does not have a stop. 
+    //        return Task.CompletedTask;
+    //    }
+    //}
 }
